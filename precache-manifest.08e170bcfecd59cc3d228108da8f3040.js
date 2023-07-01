1bd3868da20abe91ef3bb8e4a25053f1a1148c46
unknown <sheffalee123@gmail.com>
2023-07-01 23:51:31 +0530

    Updates

---
+++ precache-manifest.08e170bcfecd59cc3d228108da8f3040.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef22979c7050122913a89c6a34b8b0c",
+    "url": "/ticket-system/index.html"
+  },
+  {
+    "revision": "d5a40585e0a714cbf67c",
+    "url": "/ticket-system/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "e9c9d54d15b185f16b65",
+    "url": "/ticket-system/static/css/main.cd6da3a2.chunk.css"
+  },
+  {
+    "revision": "d5a40585e0a714cbf67c",
+    "url": "/ticket-system/static/js/2.df1b3bb3.chunk.js"
+  },
+  {
+    "revision": "eb89643b19c316b7732aae51613d6329",
+    "url": "/ticket-system/static/js/2.df1b3bb3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e9c9d54d15b185f16b65",
+    "url": "/ticket-system/static/js/main.a20e22bd.chunk.js"
+  },
+  {
+    "revision": "ee41e513464f49be052d",
+    "url": "/ticket-system/static/js/runtime-main.1aad14ba.js"
+  },
+  {
+    "revision": "74e7bd463c7fdd677d630eab5857ad15",
+    "url": "/ticket-system/static/media/logo.74e7bd46.png"
+  }
+]);
